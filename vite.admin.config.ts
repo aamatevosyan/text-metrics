b0441657aa339c0c57c1a217e71762b0a1d49a4a
Armen Matevosyan <aamatevosyan@edu.hse.ru>
2022-04-05 20:25:42 +0400

    fix: remove unused inertia vite plugin

--- vite.admin.config.ts
+++ vite.admin.config.ts
@@ -4,11 +4,9 @@ import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import laravel from 'vite-plugin-laravel'
 import vue from '@vitejs/plugin-vue'
-import inertia from './resources/scripts/vite/inertia-layout'
 
 export default defineConfig({
     plugins: [
-        inertia(),
         vue(),
         laravel({
             postcss: [
