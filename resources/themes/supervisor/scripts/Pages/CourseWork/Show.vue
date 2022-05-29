ab0c69534c60efa392fda6036ff463f8ec4e62be
Armen Matevosyan <aamatevosyan@edu.hse.ru>
2022-05-29 14:52:35 +0400

    chore: remove courseworks header

--- resources/themes/supervisor/scripts/Pages/CourseWork/Show.vue
+++ resources/themes/supervisor/scripts/Pages/CourseWork/Show.vue
@@ -1,11 +1,6 @@
 <template>
     <app-layout title="Courseworks">
         <toast/>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Courseworks
-            </h2>
-        </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
